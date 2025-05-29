d1d4de6ac2d99ae1fb7aaf4aa19fda23c4d2f7b2
Akhilesh2006s <155376342+Akhilesh2006s@users.noreply.github.com>
2025-05-29 21:13:03 +0530

    Add/update new files and folders

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,5 @@ const router = express.Router();
 // ✅ These should be routers, not controllers
 router.use("/gps", require("./gps"));
 router.use("/stops", require("./stops"));
-router.use('/attendance', require('./attendance'));
-router.use('/students', require('./student'));
 
 module.exports = router;
